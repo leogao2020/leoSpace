94d2f8e36fb87c0d41660dd371779e52332c7dee
leogao <5629409+leogao2020@user.noreply.gitee.com>
2024-04-09 18:15:25 +0800

    docker-compose.yml->think.yml add zk-single-kafka-single.yml

--- think-cola-master/think-infrastructure/src/main/java/com/amos/think/domainevent/DomainEventPublisher.java
+++ think-cola-master/think-infrastructure/src/main/java/com/amos/think/domainevent/DomainEventPublisher.java
@@ -12,9 +12,13 @@ public class DomainEventPublisher {
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
     public void publish(String topic, Object msg){
-        // publish event
-        ProducerRecord<String, Object> pr = new ProducerRecord<>(topic,msg);
-        pr.headers().add("type", msg.getClass().getName().getBytes(StandardCharsets.UTF_8));
-        kafkaTemplate.send(pr);
+        try {
+            // publish event
+            ProducerRecord<String, Object> pr = new ProducerRecord<>(topic, msg);
+            pr.headers().add("type", msg.getClass().getName().getBytes(StandardCharsets.UTF_8));
+            kafkaTemplate.send(pr);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
